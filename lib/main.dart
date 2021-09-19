a85c36107de6dedb78ec9c5a1a74613dc7f8b031
inderbhushanjha <i.jha32@gmail.com>
2021-09-20 04:09:51 +0530

    till inputText

---
+++ lib/main.dart
@@ -0,0 +1,94 @@
+import 'package:expense/transactions.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+void main() {
+  runApp(MaterialApp(
+    darkTheme: ThemeData.light(),
+    debugShowCheckedModeBanner: false,
+    home: MyApp(),
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  final List<Transaction> transaction = [
+    Transaction(
+        id: "T1", title: "ToothBrush", amount: 12.00, date: DateTime.now()),
+    Transaction(
+        id: "T2", title: "ToothPaste", amount: 69.00, date: DateTime.now()),
+  ];
+
+  MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Flutter App"),
+      ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            child: const Card(
+              child: Text(
+                "data",
+              ),
+              elevation: 5,
+            ),
+          ),
+          Column(
+            children: transaction.map((trans) {
+              return Card(
+                  elevation: 5,
+                  child: Row(
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.all(10),
+                        margin: const EdgeInsets.symmetric(
+                          vertical: 10,
+                          horizontal: 15,
+                        ),
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Colors.black,
+                            width: 2,
+                          ),
+                        ),
+                        child: Text(
+                          '₹${trans.amount}',
+                          style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: Colors.purple,
+                          ),
+                        ),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            trans.title,
+                            style: const TextStyle(
+                              fontSize: 15,
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                          Text(
+                            DateFormat('EEE, d/M/y').format(trans.date),
+                            style: const TextStyle(
+                              fontWeight: FontWeight.w500,
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  ));
+            }).toList(),
+          )
+        ],
+      ),
+    );
+  }
+}
